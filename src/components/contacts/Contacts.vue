fa55881dadca5973cbdfdadad4182802795936b0
jozue06 <jozue06@gmail.com>
2020-03-01 14:14:10 -0800

    promisify data, add delete and select to contacts

--- src/components/contacts/Contacts.vue
+++ src/components/contacts/Contacts.vue
@@ -1,63 +1,109 @@
 <template>
 	<section class="contacts">
 		<h1>Contacts</h1>
-		<b-table 
+		<!-- <b-form-group label="Selection mode:" label-cols-md="4">
+			<b-form-select v-model="selectMode" :options="modes" class="mb-3"></b-form-select>
+		</b-form-group> -->
+		<b-table  
 			striped 
 			hover 
+			ref="selectableTable"
+			selectable
 			:items="contacts" 
 			:fields="fields"
 			:sort-by.sync="sortBy"
 			:sort-desc.sync="sortDesc"
+			:select-mode="selectMode"
+			@row-selected="onRowSelected"
+			selected-variant="danger"
 			sort-icon-left
 			responsive="sm"
 			>
+			<template v-slot:cell(selected)="{ rowSelected }">
+				<template v-if="rowSelected">
+					<span aria-hidden="true">&check;</span>
+					<span variant="danger" class="sr-only">Selected</span>
+				</template>
+				<template v-else>
+					<span aria-hidden="true">&nbsp;</span>
+					<span class="sr-only">Not selected</span>
+				</template>
+			</template>
 			</b-table>
+			<b-button size="sm" @click="selectAllRows">Select all</b-button>
+			<b-button size="sm" @click="clearSelected">Clear selected</b-button>
+			<b-button variant="danger" size="sm" @click="deleteSelected">Delete selected</b-button>
 	</section>
+	
 </template>
 
 <script>
 	import "bootstrap/dist/css/bootstrap.css";
 	import "bootstrap-vue/dist/bootstrap-vue.css";
-	import { findAllContacts } from '@/data/data'
+	import { findAllContacts, deleteContacts } from '@/data/data'
 	
 	export default {
 		components: {
 		},
 		name: "Contacts",
 		methods: {
+			onRowSelected(contacts) {
+				this.selected = contacts;
+			},
+
+			selectAllRows() {
+				this.$refs.selectableTable.selectAllRows()
+			},
+
+			clearSelected() {
+				this.$refs.selectableTable.clearSelected()
+			},
+
+			deleteSelected() {
+				deleteContacts(this.selected).then(() => {
+					this.selected.forEach(selected => {
+						const index = this.contacts.findIndex(c => c.id === selected.id)
+						this.contacts.splice(index, 1)
+					});
+				});
+			}
 		},
 
 		data() {
 			let contacts = []; 
-			findAllContacts((data) => {	
+			findAllContacts().then((data) => {	
 				data.forEach(c => {
 					if (c.record.firstName && c.record.lastName) {
+						c.record.id = c._id;
 						contacts.push({...c.record});
 					}
 				});
 			});
-		
+
 			return {
 				sortBy: 'firstName',
 				sortDesc: false,
 				contacts: contacts,
-				
+				selectMode: 'multi',
 			};
 		},
+		
 		computed: {
 			fields() {
 				return Object.keys(this.contacts[0]).map(f => {
 					let tmp = {};
 					tmp.sortable = true;
-					tmp.key = f;
 
-					return tmp
+					if (f == "id"){
+						tmp.key = "";
+					} else {
+						tmp.key = f;
+
+					}
+					return tmp;
 				});
 			}
 		},
-		watch: {
-
-		}
 	};
 </script>
 
@@ -65,8 +111,5 @@
 	section {
 		float: right;
 		width: 80%;
-	}
-	.b-table > thead > th {
-		vertical-align: middle;
 	}
 </style>
--- src/data/data.js
+++ src/data/data.js
@@ -1,19 +1,36 @@
 import { remote } from 'electron';
 
-const db = remote.getGlobal('db');
+	const db = remote.getGlobal('db');
 
 
 function insertContact(record) {
-    db.insert({record});
+	db.insert({record});
 }
 
-function findAllContacts(callback) {
-    db.find({}, (err, docs) => {
-        if (err) {
-          callback(null, err);
-          return;
-        }
-        callback(docs);
-      });
+function findAllContacts() {
+	return new Promise((resolve, reject) => {
+		db.find({}, (err, docs) => {
+			if (err) {
+				reject( err);
+			} else {
+				resolve(docs);
+			}
+		});
+	});
 }
-export { insertContact, findAllContacts }
+
+function deleteContacts(contacts) {
+	return new Promise((resolve, reject) => {
+		contacts.forEach(contact => {
+			db.remove({ _id : contact.id }, (err, numDeleted) => {
+				if (err) {
+					reject(err);
+				} else {
+					resolve(numDeleted);
+				}
+			});
+		});
+	});
+}
+
+export { insertContact, findAllContacts, deleteContacts }
