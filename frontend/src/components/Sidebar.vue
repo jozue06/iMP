acca2366ae4a4555d9c61bedb7625e1fbd2fce2c
jozue06 <jozue06@gmail.com>
2020-09-13 20:27:53 -0700

    master update sidebar

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -3,9 +3,9 @@
 		<router-link to="/">
 			<h1 class="no-active">{{ sidebarTitle }}</h1>
 		</router-link>	
-		<SidebarMenuList title="Records" v-bind:itemList=sections.mainSection accordianId="collapse-1" />
-		<SidebarMenuList title="New Records" v-bind:itemList=sections.newRecordsSection accordianId="collapse-2" />
-		<SidebarMenuList title="Import & Export" v-bind:itemList=sections.importExportSection accordianId="collapse-3" />
+		<SidebarMenuList title="Menu" v-bind:itemList=sections.mainSection accordianId="collapse-1" />
+		<!-- <SidebarMenuList title="New Records" v-bind:itemList=sections.newRecordsSection accordianId="collapse-2" /> -->
+		<!-- <SidebarMenuList title="Import & Export" v-bind:itemList=sections.importExportSection accordianId="collapse-3" /> -->
 		<b-button @click="logout">
 			logout
 		</b-button>
